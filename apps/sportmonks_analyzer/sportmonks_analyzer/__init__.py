ea0ec6e5dd361daff9621036cdba5a23e259983f
coremaxti-afk <coremaxti@gmail.com>
2026-07-04 23:18:52 -0300

    feat: add SportMonks analyzer package

---
+++ apps/sportmonks_analyzer/sportmonks_analyzer/__init__.py
@@ -0,0 +1,7 @@
+"""SportMonks Analyzer App.
+
+Camada simples de leitura, normalizacao e analise exploratoria para dados JSON/CSV
+coletados no projeto LateGoalResearch.
+"""
+
+__all__ = ["loaders", "analyses"]
